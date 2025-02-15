95b69025310d8a178f6167bd425934c5caaa7db5
Harris Stevenson <harris6144@gmail.com>
2025-02-15 08:24:31 +0000

    Create environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyB91Bh4as26RvmdyiiHcUSVvxmFVfG_pkE',
+    authDomain: 'roll-away-your-stone-f7c7c.firebaseapp.com',
+    projectId: 'roll-away-your-stone-f7c7c',
+    storageBucket: 'roll-away-your-stone-f7c7c.firebasestorage.app',
+    messagingSenderId: '332582111121',
+    appId: '1:332582111121:web:f4e1cac2d5d422b2467c30',
+    measurementId: 'G-GNEW5YCN47'
+  }
+};
