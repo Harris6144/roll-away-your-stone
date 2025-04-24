1d076a8399e8acfac75e521d99fa294f360d8711
Harris Stevenson <harris6144@gmail.com>
2025-04-24 10:41:44 +0100

    Add Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    projectId: 'roll-away-your-stone-f7938',
+    appId: '1:275717248632:web:afd95a48347849bff1bf8d',
+    storageBucket: 'roll-away-your-stone-f7938.firebasestorage.app',
+    apiKey: 'AIzaSyBA80uucGAZkFCJMosQy1ySoKXfpkDBEz8',
+    authDomain: 'roll-away-your-stone-f7938.firebaseapp.com',
+    messagingSenderId: '275717248632',
+    measurementId: 'G-Z6MDSZC5LM'
+  }
+};
