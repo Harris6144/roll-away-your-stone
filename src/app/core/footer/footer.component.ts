7c375b79732809d168c536e6be8fdf0069ad08df
Harris Stevenson <harris6144@gmail.com>
2025-04-25 11:54:31 +0100

    Create footer component

---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer[app-footer]',
+  imports: [],
+  templateUrl: './footer.component.html',
+  host: {
+    'class': 'bg-body-secondary px-3 py-5'
+  }
+})
+export class FooterComponent {
+
+}
